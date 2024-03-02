0677e615a8c0317592319dd9075eabb4e7dff49d
erik suardiana <eriksuardiana644@gmail.com>
2024-03-02 10:28:33 +0800

    versi 8

--- src/app/anime/[id]/page.jsx
+++ src/app/anime/[id]/page.jsx
@@ -1,34 +1,18 @@
 import CollectionButton from "@/components/AnimeList/CollectionButton";
 import CommentInput from "@/components/AnimeList/CommentInput";
-// import CommentBox from "@/components/AnimeList/commentBox";
 import VideoPlayer from "@/components/Utilities/VideoPlayer";
 import { getAnimeResponse } from "@/libs/api-libs";
-// import { authUserSession } from "@/libs/auth-libs";
-// import prisma from "@/libs/prisma";
 import Image from "next/image";
 import React from "react";
 
 const Page = async ({ params: { id } }) => {
   const anime = await getAnimeResponse(`anime/${id}`);
-  // const user = await authUserSession();
-  // const collection = await prisma.collection.findFirst({
-  //   where: { user_email: user?.email, anime_mal_id: id },
-  // });
-  // console.log("sss", collection);
   return (
     <div>
       <div className="pt-4 px-4">
         <h3 className="text-2xl text-color-primary">
           {anime.data.title} {anime.data.year}
         </h3>
-        {/* {!collection && user && (
-          <CollectionButton
-            anime_mal_id={id}
-            user_email={user?.email}
-            anime_title={anime.data.title}
-            anime_image={anime.data.images.webp.image_url}
-          />
-        )} */}
       </div>
       <div className="pt-4 px-4 flex gap-2 text-color-primary overflow-x-auto">
         <div className="w-36 flex flex-col justify-center items-center rounded border border-color-primary p-2">
@@ -61,15 +45,6 @@ const Page = async ({ params: { id } }) => {
 
       <div className="p-4">
         <div className="text-color-primary text-2xl">Comments</div>
-        {/* <CommentBox anime_mal_id={id} /> */}
-        {/* {user && (
-          <CommentInput
-            anime_mal_id={id}
-            user_email={user?.email}
-            username={user?.name}
-            anime_title={anime.data.title}
-          />
-        )} */}
       </div>
 
       <div className="">
--- src/components/AnimeList/CommentBox.jsx
+++ src/components/AnimeList/CommentBox.jsx
@@ -1,22 +1,5 @@
-import prisma from "@/libs/prisma";
-
 const CommentBox = async ({ anime_mal_id }) => {
-  const comments = await prisma.comment.findMany({ where: { anime_mal_id } });
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4">
-      {comments.map((comment) => {
-        return (
-          <div
-            className="text-color-dark bg-color-primary p-4"
-            key={comment.id}
-          >
-            <p>{comment.username}</p>
-            <p>{comment.comment}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4"></div>;
 };
 
 export default CommentBox;
